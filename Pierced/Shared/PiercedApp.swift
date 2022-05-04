a92afbf9e0cb201e2776594b92c217008ac13e02
HabibahC <61707351+HabibahC@users.noreply.github.com>
2022-05-04 01:33:10 +0100

    Add files via upload

---
+++ Pierced/Shared/PiercedApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiercedApp.swift
+//  Shared
+//
+//  Created by Habibah Choudhrey on 23/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct PiercedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
